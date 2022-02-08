7c5745f29e3c177a8c39470ba976eb67f38adf05
Nipun753 <71968869+Nipun753@users.noreply.github.com>
2022-02-08 11:33:26 +0530

    Add files via upload

---
+++ Drop Internship/Desktop 28.js
@@ -0,0 +1,31 @@
+let w = window.innerWidth;
+gsap.registerPlugin(ScrollTrigger);
+
+if(w>600){
+    gsap.to(".parcel",{
+        y:850,
+        duration:4,
+        scrollTrigger:{
+            trigger:".parcel",
+            start:"top 20% center",
+            scrub:4,
+            toggleActions:"restart none none reverse",
+            
+        },
+        
+    })
+}
+else if(w<600) {
+    gsap.to(".parcel",{
+        y:650,
+        duration:4,
+        scrollTrigger:{
+            trigger:".parcel",
+            start:"top 20% center",
+            scrub:4,
+            toggleActions:"restart none none reverse",
+            
+        },
+        
+    })
+}
