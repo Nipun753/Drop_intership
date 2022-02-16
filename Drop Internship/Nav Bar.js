fdea65d46d56151f01efacbda26195dbe6b56e68
Nipun753 <71968869+Nipun753@users.noreply.github.com>
2022-02-16 17:45:06 +0530

    Update Nav Bar.js

--- Drop Internship/Nav Bar.js
+++ Drop Internship/Nav Bar.js
@@ -1,23 +1,29 @@
-let w = innerWidth;
-
+let w = outerWidth;
+let h=outerHeight
+let nw=(45*h)/100
 console.log(w)
+console.log(h)
+console.log(nw)
 
+let nw2=(25*w)/100
+console.log(nw2)
+let newWidth=w-nw2
+console.log(newWidth)
 function parcelMove(){
     if(w>600){
         gsap.to(".parcel",{
-            x:720,
+            x:700,
             duration:3
         })  
     }
     else{
       console.log("hy")
       gsap.to(".parcel",{
-        y:-380,
+        y:-nw,
         duration:3
     })  
     }
     
 
 }
-  
 
