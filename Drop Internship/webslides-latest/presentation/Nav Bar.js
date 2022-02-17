8f3c1c57c4dd056736bf40457527f62aefa22acd
Nipun753 <71968869+Nipun753@users.noreply.github.com>
2022-02-17 18:13:42 +0530

    Add files via upload

---
+++ Drop Internship/webslides-latest/presentation/Nav Bar.js
@@ -0,0 +1,31 @@
+let w = outerWidth;
+let h=outerHeight
+let nw=(45*h)/100
+nw=nw-10
+console.log(w)
+console.log(h)
+console.log(nw)
+
+let nw2=(25*w)/100
+console.log(nw2)
+let newWidth=w-nw2
+console.log(newWidth)
+function parcelMove(){
+    if(w>600){
+        gsap.to(".parcel",{
+            x:700,
+            duration:3
+        })  
+    }
+    else{
+      console.log("hy")
+      gsap.to(".parcel",{
+        y:-nw,
+        duration:3
+    })  
+    }
+    
+
+}
+  
+
